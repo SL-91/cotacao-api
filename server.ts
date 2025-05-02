4cc2c1f7415f6180759a4c449e3e0302a0cd5c60
ianmsfvenom <iangamer296@gmail.com>
2025-05-02 20:10:20 -0300

    first commit

--- server.ts
+++ server.ts
@@ -1,12 +1,18 @@
 import { config } from 'dotenv';
 config();
 import express from 'express';
+
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use('/api', require('./src/routes/routes'));
+
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
